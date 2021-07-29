2aaa7cd0119781e4ed8eff411473732343164699
shmirtz <karmavsnagisa101@gmail.com>
2021-07-28 21:39:23 -0500

    Revert "change screen size"
    This reverts commit 466c4115138e4343e48d6f3f60123d78d7c37ee2.

--- platformer.py
+++ platformer.py
@@ -8,8 +8,8 @@ pygame.init()
 clock = pygame.time.Clock()
 fps = 60
 
-screen_width = 900
-screen_height = 900
+screen_width = 1000
+screen_height = 1000
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Platformer')
